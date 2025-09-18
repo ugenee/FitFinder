a11df2247387cdde59e00f67bb660e6d4cd7878d
ugeneee <yooi0014@student.monash.edu>
2025-09-18 17:09:43 +0800

    feat(backend-schema): add schema for signing up and auto log in

--- backend/src/schemas/user.py
+++ backend/src/schemas/user.py
@@ -13,3 +13,16 @@ class UserCreate(BaseModel):
     user_password: str = Field(..., min_length=8)
     user_email: EmailStr
     user_age: int = Field(..., ge=16, le=80)
+
+
+class UserRead(BaseModel):
+    user_id: int
+    user_username: str
+    user_email: EmailStr
+
+    class Config:
+        from_attributes = True
+
+class UserWithToken(UserRead):
+    access_token = str
+    token_type = str
