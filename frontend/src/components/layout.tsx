eee04f3897ccc0a94b054beda0a06191d8ccaa0d
ugeneee <yooi0014@student.monash.edu>
2025-09-26 02:08:23 +0800

    fix(navbar): make navbar fixed and make sure it's on top of other components

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -6,21 +6,24 @@ export default function Layout() {
   return (
     <div className="relative min-h-screen w-full overflow-hidden bg-black">
       {/* Plasma background - positioned absolutely */}
-      <div className="fixed inset-0 w-full h-full">
+      <div className="fixed inset-0 w-full h-full z-0">
         <Plasma 
-        speed={0.5}
-        opacity={0.6}
-        scale={1.5}
-        mouseInteractive={false}
-        color="#A2AADB"
-      />
+          speed={0.5}
+          opacity={0.6}
+          scale={1.5}
+          mouseInteractive={false}
+          color="#A2AADB"
+        />
       </div>
 
       {/* Content overlay */}
       <div className="relative z-10 flex flex-col min-h-screen">
-        <div className="w-full flex justify-center mt-4">
+        {/* Navbar */}
+        <div className="fixed top-2 left-0 w-full z-50 flex justify-center">
           <Navbar />
         </div>
+
+        {/* Main content */}
         <main className="flex-1 p-4 mt-20">
           <Outlet />
         </main>
@@ -28,3 +31,4 @@ export default function Layout() {
     </div>
   );
 }
+
